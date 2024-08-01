30df8bf9675bbe183b8844ee5b9821a3c6b3acc2
Nipu Chandra Das <nipuchandradas123@gmail.com>
2024-08-02 03:00:54 +0530

    defect fixed - 3578 - fixed null values not to stored in database

--- src/main/java/com/git_examples/A.java
+++ src/main/java/com/git_examples/A.java
@@ -1,4 +1,7 @@
 package com.git_examples;
 
 public class A {
+    public static void main(String[] args) {
+        System.out.println("helo world");
+    }
 }
---
+++ src/main/java/com/git_examples/B.java
@@ -0,0 +1,7 @@
+package com.git_examples;
+
+public class B {
+    public void test() {
+        System.out.println(100);
+    }
+}
